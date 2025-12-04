58e3da43d9614528f0ba58301c937e010ea7a9a5
Agiel <agiel.negura@gmail.com>
2025-12-04 23:29:26 +0100

    Day 4

---
+++ day-04/src/bin/part2.rs
@@ -0,0 +1,64 @@
+use std::collections::BTreeSet;
+
+use aocd::*;
+
+#[aocd(2025, 4)]
+fn main() {
+    let input = input!();
+    let sum = solve(&input);
+    dbg!(sum);
+}
+
+fn solve(input: &str) -> usize {
+    let mut rolls = input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .filter_map(move |(x, c)| (c == '@').then_some((x as i32, y as i32)))
+        })
+        .collect::<BTreeSet<_>>();
+
+    let mut total = 0;
+    loop {
+        let removed = rolls
+            .iter()
+            .filter_map(|&(x, y)| {
+                (vec![
+                    (-1, -1),
+                    (-1, 0),
+                    (-1, 1),
+                    (0, -1),
+                    (0, 1),
+                    (1, -1),
+                    (1, 0),
+                    (1, 1),
+                ]
+                .iter()
+                .filter_map(|(x2, y2)| rolls.contains(&(x + x2, y + y2)).then_some(()))
+                .count()
+                    < 4)
+                .then_some((x, y))
+            })
+            .collect::<BTreeSet<_>>();
+        if removed.len() == 0 {
+            break;
+        }
+        total += removed.len();
+        rolls = rolls.difference(&removed).cloned().collect();
+    }
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = include_str!("./example1.txt");
+        assert_eq!(solve(input), 43);
+    }
+}
